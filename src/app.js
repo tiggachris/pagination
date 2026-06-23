64ff4a507e628f327e5bb0bdd298cf82135f147f
christiggaunlox <chris.tigga@unloxacademy.com>
2026-06-23 18:20:43 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
